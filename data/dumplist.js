722b93e6d18e75f64435d9f7ea92ade399078af9
monkey413 <monkey413@gmail.com>
2014-04-16 04:27:35 +0800

    half finish the package.json

--- data/dumplist.js
+++ data/dumplist.js
@@ -1,9 +1,24 @@
 console.log("hello, world");
-self.port.on("message", function(addonsNames) {
-    for (var i=0; i<addonsNames.length; ++i){
+var config = ["id", "name", "icons", "isActive", "version", 
+              "description", "homepageURL", "installDate", "isCompatible"];
+const addon = {
+    id: 0,
+    name: 1,
+    icons: 2,
+    isActive: 3,
+    version: 4,
+    description: 5,
+    homepageURL: 6,
+    installDate: 7,
+    isCompatible: 8
+};
+
+self.port.on("Config", function(addonsConfig) {
+    for (var i=0; i<addonsConfig.length; ++i){
         $("#Names").append(
-            $("<li>").append(addonsNames[i])
+            // 0:id 1: name 2: icons 3: isActive
+            (addonsConfig[i][addon.isActive]) ? $("<li>").append(addonsConfig[i][addon.name])
+                : $("<li>").append(addonsConfig[i][addon.name]).addClass("disabled")
         );
     }
 });
-
